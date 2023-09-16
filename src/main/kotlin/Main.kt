03d6cc81cde83fd80d8c3217349da3659668e866
1vnazarov <nazaroffvana.boeing@yandex.ru>
2023-09-16 15:57:46 +0300

    zadacha4

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,7 @@
 fun main() {
-    val str = readln()
-    //println(zadacha1((str)))
-    println(zadacha3(str.toInt()))
+    //println(zadacha1((readln())))
+    //println(zadacha3(readln().toInt()))
+    println(zadacha4(readln()))
 }
 
 fun zadacha1(string: String): String {
@@ -46,4 +46,33 @@ fun zadacha3(d: Int): Int {
         rank *= 10
     }
     return bin
+}
+
+fun zadacha4(string: String): Float {
+    var op = ""
+    var n1 = ""
+    var n2 = ""
+    for (char in string) {
+        if ((char != '+' || char != '-' || char != '*' || char != '/') && (n1 == "")) {
+            n1 += char
+        }
+        else {
+            if ((n2 == "") && (op != "")) {
+                n2 += char
+            }
+            else {
+                op = char.toString()
+            }
+        }
+    }
+    var res = 0.0f
+    val l = n1.toFloat()
+    val r = n2.toFloat()
+    when (op) {
+        "+" -> res = l + r
+        "-" -> res = l - r
+        "*" -> res = l * r
+        "/" -> res = l / r
+    }
+    return res
 }
